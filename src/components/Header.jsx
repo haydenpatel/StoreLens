66bde9b618d9600f3888913d3cdb84c24909ef08
Hayden Patel <hpatel@ctrlalt.nz>
2025-12-08 22:06:37 +1300

    feat: Implement #1
    Added a "Feedback" button to the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,7 +29,7 @@ export default function Header({
     <header className="bg-secondary border-b border-border sticky top-0 z-10">
     {/* <header className="bg-secondary sticky top-0 z-10"> */}
       <div className="px-6 py-4">
-        <div className="flex items-center gap-26 max-w-7xl mx-auto">
+        <div className="flex items-center gap-49">
           <div className="flex items-center gap-4">
             <div className="w-8 h-8 rounded-lg flex items-top justify-center">
               <img src="/StoreLens-icon.svg"></img>
@@ -38,7 +38,7 @@ export default function Header({
             <h1 className="text-lg font-semibold text-foreground">StoreLens</h1>
           </div>
 
-          <div className="flex-1 flex items-center gap-2 max-w-2xl">
+          <div className="flex-1 flex items-center gap-2">
             <Input
               type="text"
               placeholder="Paste Shopify collection URL (e.g., https://store.myshopify.com/collections/all)"
@@ -46,7 +46,7 @@ export default function Header({
               onChange={(e) => setCollectionUrl(e.target.value)}
               onKeyPress={handleKeyPress}
               disabled={loading}
-              className="flex-1"
+              className="flex-1 min-w-16rem"
             />
             
             {urlHistory.length > 0 && (
@@ -85,6 +85,24 @@ export default function Header({
               ) : (
                 "Load Collection"
               )}
+            </Button>
+          </div>
+          <div className="flex items-center justify-items-end gap-2">
+            <Button
+              variant="outline"
+              asChild
+              className="gap-1"
+            >
+              <a 
+                  href="https://storelens.feedbackchimp.space"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 9.75a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 0 1 .778-.332 48.294 48.294 0 0 0 5.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z" />
+                </svg>
+                Feedback
+              </a>
             </Button>
           </div>
         </div>
